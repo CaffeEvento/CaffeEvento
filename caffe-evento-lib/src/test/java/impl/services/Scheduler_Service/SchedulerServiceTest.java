18367d6dd209b13dde28a858ee3e29196e423846
EppOch <ep.perrine@gmail.com>
2016-07-27 23:21:45 -0400

    Created a new EventQueue that only sends events to the first matching handler: to be used when it is important that an event only gets handled once in case of duplicate handlers. Added an empty test for the as yet unwritten SchedulerService.

---
+++ caffe-evento-lib/src/test/java/impl/services/Scheduler_Service/SchedulerServiceTest.java
@@ -0,0 +1,16 @@
+package impl.services.Scheduler_Service;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by eric on 7/27/16.
+ */
+public class SchedulerServiceTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
