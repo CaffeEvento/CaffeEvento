faac86d187072801f646f525f2fcefaba7659cdc
chris <chrisfwin@gmail.com>
2016-08-21 14:31:06 -0400

    an example of the request service being able to be registered.

--- caffe-evento-core/src/main/java/com/cmbellis/caffeevento/core/main/CoreApplication.java
+++ caffe-evento-core/src/main/java/com/cmbellis/caffeevento/core/main/CoreApplication.java
@@ -1,22 +1,15 @@
 package com.cmbellis.caffeevento.core.main;
 
-import com.cmbellis.caffeevento.lib.annotation.CEExport;
 import com.cmbellis.caffeevento.lib.api.services.Service;
 import org.apache.felix.framework.FrameworkFactory;
 import org.apache.felix.main.AutoProcessor;
 import org.osgi.framework.*;
 import org.osgi.framework.launch.Framework;
-import org.reflections.Reflections;
-import org.reflections.util.ClasspathHelper;
-import org.reflections.util.ConfigurationBuilder;
 
-import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -24,69 +17,58 @@ import java.util.stream.Stream;
  */
 public class CoreApplication {
 
-    private static ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+//    private static ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
 
 
-    public static void main(String[] argv) throws Exception
-    {
-        // Print welcome banner.
-        System.out.println("\nWelcome to CaffeEvento");
-        System.out.println("Powered by Apache Felix!");
-        System.out.println("======================\n");
-
-        Reflections reflections = new Reflections("com.cmbellis");
-
-        Set<Class<?>> allObjects = reflections.getTypesAnnotatedWith(CEExport.class, true);
-        List<String> packages = allObjects.stream()
-                .map(Class::getPackage)
-                .map(Package::getName)
-                .distinct()
-                .sorted()
-                .collect(Collectors.toList());
-
-        packages.add("org.apache.commons.logging; version=1.2.0");
-
-        String systemPackages = packages.stream().collect(Collectors.joining(","));
-
-        System.out.println("System Packages: ");
-        Stream.of(systemPackages.split(",")).forEach(System.out::println);
-
-        Properties configProps = new Properties();
-        configProps.setProperty(AutoProcessor.AUTO_DEPLOY_DIR_PROPERTY, "./bundle");
-        configProps.setProperty(AutoProcessor.AUTO_DEPLOY_ACTION_PROPERTY, "install,update,start,uninstall");
-        configProps.setProperty(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
-        configProps.setProperty(Constants.FRAMEWORK_STORAGE, "cache");
-        configProps.setProperty(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, systemPackages);
-
-        try
-        {
-            final Framework framework = new FrameworkFactory().newFramework(configProps);
-            framework.init();
-            AutoProcessor.process(configProps, framework.getBundleContext());
-
-//            framework.getBundleContext().addBundleListener(bundleEvent -> {
-//                if(bundleEvent.getBundle() != null) {
-//                    for(ServiceReference<?> serviceReference : bundleEvent.getBundle().getRegisteredServices()) {
-//                        final ServiceObjects<?> serviceObjects =
-//                                bundleEvent.getBundle().getBundleContext().getServiceObjects(serviceReference);
-//                        Object s = serviceObjects.getService();
-//                        if(s instanceof Service) {
-//                            Service newService = (Service)s;
-//                            System.out.println("A service event occurred on: " + newService.getClass());
-//                        }
-//                    }
-//                }
-//            });
-
-            framework.start();
-            framework.waitForStop(0);
-            System.exit(0);
-        }
-        catch (Exception ex)
-        {
-            System.err.println("Could not create framework: " + ex);
-            ex.printStackTrace();
-            System.exit(-1);
-        }
-    }
+//    public static void main(String[] argv) throws Exception
+//    {
+//        // Print welcome banner.
+//        System.out.println("\nWelcome to CaffeEvento");
+//        System.out.println("Powered by Apache Felix!");
+//        System.out.println("======================\n");
+//
+//        Properties configProps = new Properties();
+//        configProps.setProperty(AutoProcessor.AUTO_DEPLOY_DIR_PROPERTY, "./bundle");
+//        configProps.setProperty(AutoProcessor.AUTO_DEPLOY_ACTION_PROPERTY, "install,update,start,uninstall");
+//        configProps.setProperty(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
+//        configProps.setProperty(Constants.FRAMEWORK_STORAGE, "cache");
+//        configProps.setProperty(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA,
+//                "com.cmbellis.caffeevento.lib.api.events," +
+//                "com.cmbellis.caffeevento.lib.api.events.event_queue," +
+//                "com.cmbellis.caffeevento.lib.api.events.event_queue.event_queue_interface," +
+//                "com.cmbellis.caffeevento.lib.api.lib," +
+//                "com.cmbellis.caffeevento.lib.api," +
+//                );
+//
+//        try
+//        {
+//            final Framework framework = new FrameworkFactory().newFramework(configProps);
+//            framework.init();
+//            AutoProcessor.process(configProps, framework.getBundleContext());
+//
+////            framework.getBundleContext().addBundleListener(bundleEvent -> {
+////                if(bundleEvent.getBundle() != null) {
+////                    for(ServiceReference<?> serviceReference : bundleEvent.getBundle().getRegisteredServices()) {
+////                        final ServiceObjects<?> serviceObjects =
+////                                bundleEvent.getBundle().getBundleContext().getServiceObjects(serviceReference);
+////                        Object s = serviceObjects.getService();
+////                        if(s instanceof Service) {
+////                            Service newService = (Service)s;
+////                            System.out.println("A service event occurred on: " + newService.getClass());
+////                        }
+////                    }
+////                }
+////            });
+//
+//            framework.start();
+//            framework.waitForStop(0);
+//            System.exit(0);
+//        }
+//        catch (Exception ex)
+//        {
+//            System.err.println("Could not create framework: " + ex);
+//            ex.printStackTrace();
+//            System.exit(-1);
+//        }
+//    }
 }
---
+++ caffe-evento-lib/src/main/java/com/cmbellis/caffeevento/lib/Application.java
@@ -0,0 +1,89 @@
+package com.cmbellis.caffeevento.lib;
+
+import com.cmbellis.caffeevento.lib.annotation.CEExport;
+import com.cmbellis.caffeevento.lib.api.services.Service;
+import org.apache.felix.framework.FrameworkFactory;
+import org.apache.felix.main.AutoProcessor;
+import org.osgi.framework.Constants;
+import org.osgi.framework.ServiceObjects;
+import org.osgi.framework.ServiceReference;
+import org.osgi.framework.launch.Framework;
+import org.reflections.Reflections;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.stream.Collectors;
+
+/**
+ * Created by chris on 8/21/16.
+ */
+public class Application {
+
+    private static ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
+
+    public static void main(String[] argv) throws Exception
+    {
+        // Print welcome banner.
+        System.out.println("\nWelcome to CaffeEvento");
+        System.out.println("Powered by Apache Felix!");
+        System.out.println("======================\n");
+
+        Reflections reflections = new Reflections("com.cmbellis.caffeevento");
+        List<String> libPackages = reflections
+                .getTypesAnnotatedWith(CEExport.class)
+                .stream()
+                .map(Class::getPackage)
+                .map(Package::getName)
+                .collect(Collectors.toList());
+
+        List<String> providedPackages = Arrays.asList("org.apache.commons.logging;version=1.2.0");
+        libPackages.addAll(providedPackages);
+
+        String export = libPackages.stream()
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining(","));
+
+        Properties configProps = new Properties();
+        configProps.setProperty(AutoProcessor.AUTO_DEPLOY_DIR_PROPERTY, "./bundle");
+        configProps.setProperty(AutoProcessor.AUTO_DEPLOY_ACTION_PROPERTY, "install,update,start,uninstall");
+        configProps.setProperty(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
+        configProps.setProperty(Constants.FRAMEWORK_STORAGE, "cache");
+        configProps.setProperty(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, export);
+
+        try
+        {
+            final Framework framework = new FrameworkFactory().newFramework(configProps);
+            framework.init();
+            AutoProcessor.process(configProps, framework.getBundleContext());
+
+            framework.getBundleContext().addBundleListener(bundleEvent -> {
+                if(bundleEvent.getBundle() != null && bundleEvent.getBundle().getRegisteredServices() != null) {
+                    for(ServiceReference<?> serviceReference : bundleEvent.getBundle().getRegisteredServices()) {
+                        final ServiceObjects<?> serviceObjects =
+                                bundleEvent.getBundle().getBundleContext().getServiceObjects(serviceReference);
+                        Object s = serviceObjects.getService();
+                        if(s instanceof Service) {
+                            Service newService = (Service)s;
+                            System.out.println("A service event occurred on: " + newService.getClass());
+                        }
+                    }
+                }
+            });
+
+            framework.start();
+            framework.waitForStop(0);
+            System.exit(0);
+        }
+        catch (Exception ex)
+        {
+            System.err.println("Could not create framework: " + ex);
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+}
