46b17abeb5d70d59f37f081245c93960f6200608
chris <chrisfwin@gmail.com>
2016-08-21 14:40:52 -0400

    Removed core, because it was not needed anymore.

--- caffe-evento-core/src/main/java/com/cmbellis/caffeevento/core/main/CoreActivator.java
+++
@@ -1,7 +0,0 @@
-package com.cmbellis.caffeevento.core.main;
-
-/**
- * Created by chris on 8/10/16.
- */
-public class CoreActivator {
-}
--- caffe-evento-lib/src/main/java/com/cmbellis/caffeevento/lib/Application.java
+++ caffe-evento-lib/src/main/java/com/cmbellis/caffeevento/lib/Application.java
@@ -4,6 +4,7 @@ import com.cmbellis.caffeevento.lib.annotation.CEExport;
 import com.cmbellis.caffeevento.lib.api.services.Service;
 import org.apache.felix.framework.FrameworkFactory;
 import org.apache.felix.main.AutoProcessor;
+import org.osgi.framework.BundleEvent;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceObjects;
 import org.osgi.framework.ServiceReference;
@@ -13,24 +14,19 @@ import org.reflections.Reflections;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
 
 /**
  * Created by chris on 8/21/16.
  */
 public class Application {
-
-    private static ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
-
-    public static void main(String[] argv) throws Exception
-    {
+    public static void main(String[] argv) throws Exception {
         // Print welcome banner.
         System.out.println("\nWelcome to CaffeEvento");
         System.out.println("Powered by Apache Felix!");
         System.out.println("======================\n");
 
+        // Get all the lib stuff together
         Reflections reflections = new Reflections("com.cmbellis.caffeevento");
         List<String> libPackages = reflections
                 .getTypesAnnotatedWith(CEExport.class)
@@ -39,6 +35,7 @@ public class Application {
                 .map(Package::getName)
                 .collect(Collectors.toList());
 
+        // Provide a few packages that might be used by services
         List<String> providedPackages = Arrays.asList("org.apache.commons.logging;version=1.2.0");
         libPackages.addAll(providedPackages);
 
@@ -54,21 +51,23 @@ public class Application {
         configProps.setProperty(Constants.FRAMEWORK_STORAGE, "cache");
         configProps.setProperty(Constants.FRAMEWORK_SYSTEMPACKAGES_EXTRA, export);
 
-        try
-        {
+        try {
             final Framework framework = new FrameworkFactory().newFramework(configProps);
             framework.init();
             AutoProcessor.process(configProps, framework.getBundleContext());
 
             framework.getBundleContext().addBundleListener(bundleEvent -> {
-                if(bundleEvent.getBundle() != null && bundleEvent.getBundle().getRegisteredServices() != null) {
-                    for(ServiceReference<?> serviceReference : bundleEvent.getBundle().getRegisteredServices()) {
+                if (bundleEvent.getBundle() != null && bundleEvent.getBundle().getRegisteredServices() != null) {
+                    for (ServiceReference<?> serviceReference : bundleEvent.getBundle().getRegisteredServices()) {
                         final ServiceObjects<?> serviceObjects =
                                 bundleEvent.getBundle().getBundleContext().getServiceObjects(serviceReference);
                         Object s = serviceObjects.getService();
-                        if(s instanceof Service) {
-                            Service newService = (Service)s;
-                            System.out.println("A service event occurred on: " + newService.getClass());
+
+                        if (s instanceof Service) {
+                            if(bundleEvent.getType() == BundleEvent.STARTED) {
+                                Service newService = (Service) s;
+                                System.out.println("Service started: " + newService.getClass());
+                            }
                         }
                     }
                 }
@@ -77,9 +76,7 @@ public class Application {
             framework.start();
             framework.waitForStop(0);
             System.exit(0);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             System.err.println("Could not create framework: " + ex);
             ex.printStackTrace();
             System.exit(-1);
